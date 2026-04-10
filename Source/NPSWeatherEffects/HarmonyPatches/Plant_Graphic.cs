e5c8b83759e8503b355125cf3b1a1c24c249bfbc
Halicade <qstr23@hotmail.com>
2026-04-10 18:10:44 -0400

    Added plant patch back in. Should be off by default

--- Source/NPSWeatherEffects/HarmonyPatches/Plant_Graphic.cs
+++ Source/NPSWeatherEffects/HarmonyPatches/Plant_Graphic.cs
@@ -11,17 +11,15 @@ public static class Plant_Graphic
 {
     private static Map cachedMap;
     private static Watcher watcher;
-    private static Season season;
 
     //TODO think about immature plant growth levels
-    
+
     //Giving higher priority to be before any mods that may modify texture
     [HarmonyPriority(Priority.High)]
     public static void Postfix(Plant __instance, ref Graphic __result) {
         if (cachedMap != __instance.Map) {
             cachedMap = __instance.Map;
             watcher = cachedMap.GetComponent<Watcher>();
-            season = watcher.season;
         }
 
         if (watcher?.dontRunAnything != false) {
@@ -33,39 +31,37 @@ public static class Plant_Graphic
             return;
         }
 
-        if (Find.TickManager.TicksAbs % 60000 == 0) {
-            //Check every day for if the season changed
-            season = watcher.season;
-        }
-
-
+        var temperature = __instance.Position.GetTemperature(cachedMap);
         //get flowering or drought graphic if it's over 70
-        if (__instance.AmbientTemperature > 21) {
-            if (watcher.cellWeatherAffects.TryGetValue(__instance.Position, out var cell)) {
-                if ((cell.howWetPlants > 60 && cachedMap.weatherManager.RainRate <= .001f || season == Season.Spring)
-                    && graphics.floweringGraphicPath != null) {
-                    __result = graphics.floweringGraphic;
+        if (temperature > 21) {
+            if ((cachedMap.weatherManager.RainRate <= .001f && watcher.season == Season.Spring)
+                && graphics.floweringGraphicPath != null) {
+                __result = graphics.floweringGraphic;
+                return;
+            }
+
+            if (watcher.droughtActive) {
+                if (graphics.droughtGraphicPath != null) {
+                    __result = graphics.droughtGraphic;
                     return;
                 }
 
-                if (cell.howWetPlants < 20) {
-                    if (graphics.droughtGraphicPath != null) {
-                        __result = graphics.droughtGraphic;
-                        return;
-                    }
-
-                    if (__instance.def.plant.leaflessGraphic != null) {
-                        __result = __instance.def.plant.leaflessGraphic;
-                        return;
-                    }
+                if (__instance.def.plant.leaflessGraphic != null) {
+                    __result = __instance.def.plant.leaflessGraphic;
+                    return;
                 }
             }
 
-            //There won't be snow if it's this warm so no need to check the below
             return;
         }
 
-        if (!EffectSettings.showFrostGrid) return;
+        if (!EffectSettings.showFrostGrid) {
+            return;
+        }
+
+        if (temperature > 0) {
+            return;
+        }
 
         if (graphics.frostGraphicPath != null && watcher.frostGridComponent.GetDepth(__instance.Position) >= 0.6f) {
             __result = graphics.frostGraphic;
