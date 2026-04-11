ec58fa22950b3bdd45f9912ac1c5a98b22b06c03
Halicade <qstr23@hotmail.com>
2026-04-11 13:49:45 -0400

    When I added the drought stuff and forgot to commit the rest

---
+++ Source/NPSWeatherEffects/DefOfs/GameConditionDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace NPSWeather;
+
+[DefOf]
+public class GameConditionDefOf
+{
+    public static GameConditionDef TKKN_Drought;
+    
+    static GameConditionDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(GameConditionDefOf));
+    }
+}
