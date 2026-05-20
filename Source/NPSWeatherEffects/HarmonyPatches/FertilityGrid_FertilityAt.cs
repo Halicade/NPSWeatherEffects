5027e34b17498409c4dd21ace21f345aae86cfdb
Halicade <qstr23@hotmail.com>
2026-05-19 21:45:06 -0400

    Better checks when using multiple maps for fertility modifier

--- Source/NPSWeatherEffects/HarmonyPatches/FertilityGrid_FertilityAt.cs
+++ Source/NPSWeatherEffects/HarmonyPatches/FertilityGrid_FertilityAt.cs
@@ -2,26 +2,29 @@
 
 namespace NPSWeather;
 
-
 /// <summary>
 /// This method feels too expensive to use
 /// </summary>
 public class FertilityGrid_FertilityAt
 {
-    public static Map cachedMap;
-    public static Watcher cachedWatcher;
-    
+    private static Map _cachedMap;
+    private static Watcher _cachedWatcher;
+
     public static void Postfix(Map ___map, IntVec3 loc, ref float __result) {
-        if (___map != cachedMap) {
-            cachedMap = ___map;
-            cachedWatcher = cachedMap.GetComponent<Watcher>();
+        if (___map != _cachedMap) {
+            if (EffectSettings.onlyPlayerHome && 
+                ___map?.IsPlayerHome != true) {
+                return;
+            }
+
+            _cachedMap = ___map;
+            _cachedWatcher = _cachedMap.GetComponent<Watcher>();
         }
 
-        if (cachedWatcher.isRaining) {
-            if (cachedWatcher.cellWeatherAffects.TryGetValue(loc, out var rainIncrease)) {
+        if (_cachedWatcher.isRaining) {
+            if (_cachedWatcher.cellWeatherAffects.TryGetValue(loc, out var rainIncrease)) {
                 __result *= 1 + rainIncrease.rainLevel;
             }
-            
         }
     }
 }
